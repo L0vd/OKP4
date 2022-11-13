3af3694a0ef1eb659f4f52a39cc1a0f5df97395e
NktSH <49846495+NktSH@users.noreply.github.com>
2022-11-14 00:37:03 +0300

    Create variables.sh

---
+++ Monitoring/variables.sh
@@ -0,0 +1,5 @@
+#OKP4 monitoring variables template 
+COS_BIN_NAME=insert_path_to_nibid_binary # example: /root/go/bin/okp4d or /home/user/go/bin/okp4d
+COS_PORT_RPC=insert_port_here              # default: 26657
+COS_VALOPER=insert_valoper_address         # example: okp4valoper1234545636767376535673
+LOGENTRY=okp4
